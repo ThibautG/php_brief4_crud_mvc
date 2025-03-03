ed4cc824dca3f20e2145d6ed62d6fa1e2fbebb65
ThibautG <thibaut.gaudinat@gmail.com>
2025-03-03 17:38:00 +0100

    First Commit

---
+++ views/footer.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<footer>
+    <p>C'est le Footer de MyTech</p>
+</footer>
+</body>
+</html>
---
+++ views/productDetails.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.php'
+?>
+
+<h2>Coucou les produits</h2>
+
+<?php
+include 'footer.php'
+?>
